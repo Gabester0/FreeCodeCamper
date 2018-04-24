a4a8c0fb677eca1e12b2990a5ea67c8496e57335
Gabe Eipper <30967371+Gabester0@users.noreply.github.com>
2018-04-24 09:53:29 -0400

    Create repeatStringXTimes.js

---
+++ BasicAlgorithm/repeatStringXTimes.js
@@ -0,0 +1,12 @@
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  var Willi = [];
+  for(x=0; x<num; x++){
+  Willi.push(str);
+ 
+  }
+return Willi.join(""); 
+}
+
+repeatStringNumTimes("Lollipop", 10);
