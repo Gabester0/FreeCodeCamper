d171e294b524b3afa0fd1b0ef1fa833355c54d99
Gabe Eipper <30967371+Gabester0@users.noreply.github.com>
2018-04-24 09:59:22 -0400

    Create whereDoIBelong.js
    Completed 3/10/18

---
+++ BasicAlgorithm/whereDoIBelong.js
@@ -0,0 +1,13 @@
+
+
+function getIndexToIns(arr, num) {
+  // Find my place in this sorted array.
+  var ark = arr.sort();
+  if ((ark[0]>=20) && (ark[0]> ark[1])) { ark.push(ark[0]); ark.shift(); }
+  var x=0;
+  for (x; ark[x] < num; x++){
+    if (ark[x] < num){  } else if(ark[x]===num){ x=x-1; } else { return x; } }
+ return x;
+}
+
+getIndexToIns([5, 3, 20, 3], 5);
