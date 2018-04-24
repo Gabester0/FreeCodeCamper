a78f04595df761c72d551b8817fb9058cfb26d46
Gabe Eipper <30967371+Gabester0@users.noreply.github.com>
2018-04-24 09:57:23 -0400

    Create mutations.js
    Completed 3/8/18

---
+++ BasicAlgorithm/mutations.js
@@ -0,0 +1,13 @@
+
+function mutation(arr) {
+  var a = arr[0].toLowerCase();
+  var b = arr[1].toLowerCase();
+  var k = [];
+  
+  for (x = 0; x<b.length; x++) {
+  k.push(a.indexOf(b[x]));   
+  }
+  
+  return ((k.indexOf(-1)) === -1);
+}
+mutation(["Hello", "hello"]);
