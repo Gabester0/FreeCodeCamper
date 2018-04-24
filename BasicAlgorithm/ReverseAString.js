c972f7853527987d68ad3aa67c2bbd78bbe0b374
Gabe Eipper <30967371+Gabester0@users.noreply.github.com>
2018-04-23 23:28:53 -0400

    Create ReverseAString.js

---
+++ BasicAlgorithm/ReverseAString.js
@@ -0,0 +1,22 @@
+Reverse a String
+
+function reverseString(str) {
+//use .length to ascertain how many char are in str//x//
+  //use for loop to assign each char in string as a separate [] elements in a
+  //array
+  //Then reverse the order of these 
+  //then return these to string form and display. 
+// str.length = x;
+/*  for (x = 0; x < (str.length); x++){
+  }*/
+  var reversed = str.split("");
+
+  var flipString = [];
+  
+  flipString = reversed.reverse();
+  
+  return flipString.join("");
+  
+}
+
+reverseString("A Man a Plan A Canal Panama");
