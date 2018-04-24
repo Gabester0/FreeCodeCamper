68813b2f5398d69ac5059bbc3b4121588ed95575
Gabe Eipper <30967371+Gabester0@users.noreply.github.com>
2018-04-23 23:30:13 -0400

    Create FactorializeaNumber.js

---
+++ BasicAlgorithm/FactorializeaNumber.js
@@ -0,0 +1,11 @@
+function factorialize(num) {
+  var fac = 1;
+  for (x = num; x > 0; x--){
+  
+    fac *= x;
+  }
+  return fac;
+  //Now we need to multiply each array entry against itself and return the result
+}
+
+factorialize(5);
