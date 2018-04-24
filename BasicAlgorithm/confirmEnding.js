9f0ae441891a8769ab0ce62fb39f1d7066103ffe
Gabe Eipper <30967371+Gabester0@users.noreply.github.com>
2018-04-24 09:52:33 -0400

    Create confirmEnding.js

---
+++ BasicAlgorithm/confirmEnding.js
@@ -0,0 +1,22 @@
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+var bar = target.length -1;
+  str = str.split(" ");
+    var l = str.length -1;
+    var m = str[l].length-1;
+    var q = str[l];
+    q = q.split("");
+  for (m; m> bar; m-- ){
+    q.shift();
+  }
+  
+  q = q.join("");
+  
+if (q === target) {
+  return true; } 
+  else { return false; } 
+  
+} 
+
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification");
